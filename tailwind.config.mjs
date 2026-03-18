3fa82fca555b6f920bd9495e1c1ac20a0dc98b5e
Wijnand Baretta <wijnandb@retta.nl>
2026-03-18 22:07:47 +0100

    Initial IGA website - Astro + Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'iga-blue': '#95BCF8',
+        'iga-grey': '#E1E1E1',
+        'iga-black': '#000000',
+        'iga-white': '#FFFFFF',
+        'iga-blue-dark': '#6B9BE8',
+        'iga-blue-light': '#B8D4FB',
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'Georgia', 'serif'],
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
