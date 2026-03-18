d234078ce6411b69b407fe623dd93372d1a872da
Wijnand Baretta <wijnandb@retta.nl>
2026-03-18 22:22:52 +0100

    Add services, about, contact, 404 pages; SEO improvements; Formbridge contact form; fix image paths for base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://perceptumnl.github.io',
   base: '/iga-website',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
